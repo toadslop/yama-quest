8d6dcf822368a61d197193c0f22b59719c1c2279
Brian Heise <bnheise@gmail.com>
2020-04-26 18:30:16 +0900

    made api route to make react mountain list show

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#landing'
+
+  # API routing
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :lists, only: [:show]
+    end
+  end
 end
