fc5480150f42bf2b3000b28eea184f9516ebd970
Brian Heise <bnheise@gmail.com>
2020-04-29 21:03:52 +0900

    refactored some code

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,25 +1,19 @@
 # frozen_string_literal: true
 
 class ListsController < ApplicationController
-  def index
-    @lists = List.all
-  end
+  before_action :fetch_lists, only: [:index]
+  before_action :fetch_list, only: [:show]
+  before_action :fetch_mountains, only: [:show]
+  before_action :fetch_regions, only: [:show]
 
-  def show
-    if List.exists?(id: params[:id])
-      @list = List.find(params[:id])
-      @regions = mountains_by_region(@list)
-    else
-      redirect_to list_path(List.first)
-    end
-  end
+  def index; end
+
+  def show; end
 
   private
 
-  def mountains_by_region(list)
-    mountains = list.mountains.includes([:region])
-    regions = {}
-    mountains.each do |mountain|
+  def mountains_by_region
+    @mountains.each do |mountain|
       if regions.key?(mountain.region.name)
         regions[mountain.region.name]['mountains'] << mountain
       else
@@ -28,6 +22,25 @@ class ListsController < ApplicationController
         regions[mountain.region.name]['mountains'] = [mountain]
       end
     end
-    regions
+  end
+
+  def fetch_lists
+    @lists = List.all
+  end
+
+  def fetch_list
+    if List.exists?(id: params[:id])
+      @list = List.find(params[:id])
+    else
+      redirect_to list_path(List.first)
+    end
+  end
+
+  def fetch_regions
+    @regions = mountains_by_region
+  end
+
+  def fetch_mountains
+    @mountains = @list.mountains.includes([:region])
   end
 end
