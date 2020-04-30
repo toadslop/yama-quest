0b517825e06a3250bb30d2a8c6e7383c40b4c02a
Brian Heise <bnheise@gmail.com>
2020-05-01 06:59:50 +0900

    deleted some uneccessary code

--- app/models/mountain.rb
+++ app/models/mountain.rb
@@ -2,8 +2,5 @@ class Mountain < ApplicationRecord
   belongs_to :region, -> { select(:id, :name) }
   has_many :list_mountains
   has_many :lists, through: :list_mountains
-
-  def self.list
-    Mountain.select(:name, :region_id).includes(:region)
-  end
 end
+
