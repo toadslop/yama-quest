068eb2cc04704cea5089f2d8be447d07baac039c
Brian Heise <bnheise@gmail.com>
2020-05-01 06:58:19 +0900

    created instance method for list to get the sidebar data without eager loading or exposing data unnecessarily

--- app/models/list.rb
+++ app/models/list.rb
@@ -4,7 +4,11 @@
 
 class List < ApplicationRecord
   validates :name, presence: true
-  has_many :list_mountains
-  has_many :mountains, through: :list_mountains
-  has_many :regions, through: :mountains
+  has_many :list_mountains, -> { select(:id, :number, :mountain_id) }
+  has_many :mountains, -> { select(:name, :region_id) }, through: :list_mountains
+  has_many :regions, -> { select(:id, :name) }, through: :mountains
+
+  def sidebar_data
+    list_mountains.includes(:mountain).includes(:region)
+  end
 end
--- app/models/list_mountain.rb
+++ app/models/list_mountain.rb
@@ -1,5 +1,10 @@
 class ListMountain < ApplicationRecord
   belongs_to :list
-  belongs_to :mountain
+  belongs_to :mountain, -> { select(:id, :name, :region_id) }
+  # belongs_to :list_data, -> { select(:id, :name, :region_id) }, class_name: 'Mountain', foreign_key: 'mountain_id'
   has_one :region, through: :mountain
+
+  def list_data
+    mountain.select(:name, :region_id)
+  end
 end
