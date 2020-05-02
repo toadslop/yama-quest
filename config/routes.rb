72cbc1a3f9d0e8733b59cc5b3446d763b22ff6a9
Brian Heise <bnheise@gmail.com>
2020-05-03 01:00:44 +0900

    localization configured

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'pages#landing'
-  resources :lists, only: [:index, :show]
-  resources :mountains, only: [:index, :show]
+  scope '(:locale)', locale: /jp/ do
+    root to: 'pages#landing'
+    resources :lists, only: [:index, :show]
+    resources :mountains, only: [:index, :show]
+  end
 
   # API routing
   namespace :api, defaults: { format: :json } do
