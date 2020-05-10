f73683f033de2335a35490d9907d3f23b2963dbc
Brian Heise <bnheise@gmail.com>
2020-05-10 15:33:01 +0900

    added dotenv for javascript and configured environment.js

--- app/javascript/list/containers/MountainMap.jsx
+++ app/javascript/list/containers/MountainMap.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import mapboxgl from 'mapbox-gl';
 
 class MountainMap extends Component {
   render() {
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,16 @@
 const { environment } = require('@rails/webpacker')
 const erb = require('./loaders/erb')
+const dotenv = require('dotenv')
+
+const dotenvFiles = [
+  `.env.${process.env.NODE_ENV}.local`,
+  '.env.local',
+  `.env.${process.env.NODE_ENV}`,
+  '.env'
+]
+dotenvFiles.forEach((dotenvFile) => {
+  dotenv.config({ path: dotenvFile, silent: true })
+})
 
 environment.loaders.prepend('erb', erb)
 module.exports = environment
