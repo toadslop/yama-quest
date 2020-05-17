2c78cbfcb6020bf7a74c30216f29e6a29f0be4e9
Brian Heise <bnheise@gmail.com>
2020-05-17 10:05:53 +0900

    created class methods for generating geojson files from a list

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,13 +2,27 @@
 
 # This class defines lists of mountains, like the 100 famous mountains.
 
+GEOJSON_TEMPLATE = {
+  type: '',
+  features: []
+}
+
 class List < ApplicationRecord
   validates :name, presence: true
   has_many :list_mountains, -> { select(:id, :number, :mountain_id) }
-  has_many :mountains, -> { select(:name, :region_id) }, through: :list_mountains
+  has_many :mountains, through: :list_mountains #, -> { select(:name, :region_id) },
   has_many :regions, -> { select(:id, :name) }, through: :mountains
 
   def regions_list
-    region_list = regions.distinct.select(:name, :id).order(:id)
+    @region_list = regions.distinct.select(:name, :id).order(:id)
+  end
+
+  def feature_collection
+    @collection = GEOJSON_TEMPLATE
+    @collection[:type] = 'FeatureCollection'
+    mountains.each do |mountain|
+      @collection[:features] << mountain.geojson_feature
+    end
+    @collection
   end
 end
--- app/models/mountain.rb
+++ app/models/mountain.rb
@@ -1,6 +1,31 @@
+FEATURE_TEMPLATE = {
+  type: '',
+  geometry: {
+    type: '',
+    coordinates: [0, 0]
+  },
+  properties: {
+    title: '',
+    description: ''
+  }
+}
+
 class Mountain < ApplicationRecord
   belongs_to :region, -> { select(:id, :name) }
   has_many :list_mountains
   has_many :lists, through: :list_mountains
-end
 
+  def geojson_feature
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [lng, lat]
+      },
+      properties: {
+        title: name,
+        description: name
+      }
+    }
+  end
+end
