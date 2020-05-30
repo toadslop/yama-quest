6f744c8c71d4c8ecdedd08492963446453f27def
Brian Heise <bnheise@gmail.com>
2020-05-30 14:47:08 +0900

    popups render and swipe works on mobile, but img doesn't update when clicking a different marker

--- app/javascript/list/components/MountainInfo.jsx
+++ app/javascript/list/components/MountainInfo.jsx
@@ -1,40 +1,93 @@
 import React, { Component } from 'react';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
 import withSizes from 'react-sizes';
+import { useSwipeable, Swipeable } from 'react-swipeable'
 
 class MountainInfo extends Component {
-  renderInfo = () => {
-    const { info } = this.props;
+  constructor(props) {
+    super(props);
+    this.state = {
+      onInfo: true
+    }
+  }
+
+  renderInfo = (info) => {
     const { title, description } = info.properties;
     const { altitude, terrain, effort, length } = description;
 
-    return (
-      <div>
-        <h3>{I18n.t(`mountains.${title}`)}</h3>
-        <p>{I18n.t(`attributes.altitude`)}: {altitude}m</p>
-        <p>{I18n.t(`attributes.terrain`)}: {terrain}</p>
-        <p>{I18n.t(`attributes.effort`)}: {effort}</p>
-        <p>{I18n.t(`attributes.length`)}: {I18n.t(`lengths.${length}`)}</p>
-      </div>
-    )
+    if (this.state.onInfo) {
+      return (
+        <div>
+          <h3>{I18n.t(`mountains.${title}`)}</h3>
+          <p>{I18n.t(`attributes.altitude`)}: {altitude}m</p>
+          <p>{I18n.t(`attributes.terrain`)}: {terrain}</p>
+          <p>{I18n.t(`attributes.effort`)}: {effort}</p>
+          <p>{I18n.t(`attributes.length`)}: {I18n.t(`lengths.${length}`)}</p>
+        </div>
+      )
+    } else {
+      return (
+        <div className="popup-content">
+          <img className={imgClass} src={img_url} />
+        </div>
+      )
+    }
   }
   
-  renderDesktopInfo = () => {
-    const {img_url} = this.props.info.properties.description;
+  renderDesktopInfo = (info) => {
+    const { img_url } = info.properties.description;
     const imgClass = (img_url ? 'popup-img' : 'img-hidden')
 
     return (
       <div className="popup-content">
-        {this.renderInfo()}
+        {this.renderInfo(info)}
         <img className={imgClass} src={img_url} />
       </div>
     )
   }
 
-  render() {
+  renderImg = (info) => {
+    const { img_url } = info.properties.description;
+    const imgClass = 'popup-img'
     return (
-      <div>{this.props.isMobile ? this.renderInfo() : this.renderDesktopInfo()}</div>
+      <div className="popup-content">
+        <img className={imgClass} src={img_url} />
+      </div>
+    )
+  }
+
+  leftSwipe = (info) => {
+    const content = this.renderImg(info)
+    this.setState({ mobileContent: content, onInfo: false })
+  }
+
+  rightSwipe = (info) => {
+    const content = this.renderInfo(info);
+    this.setState({mobileContent: content, onInfo: true })
+  }
+
+  renderMobileInfo = (info) => {
+    const config = {
+      delta: 10,                             // min distance(px) before a swipe starts
+      preventDefaultTouchmoveEvent: false,   // preventDefault on touchmove, *See Details*
+      trackTouch: true,                      // track touch input
+      trackMouse: false,                     // track mouse input
+      rotationAngle: 0,                      // set a rotation angle
+    }
+
+    return (
+      <Swipeable
+        onSwipedLeft={() => this.leftSwipe(info) }
+        onSwipedRight={() => this.rightSwipe(info) }
+        {...config} >
+        {this.state.mobileContent || this.renderInfo(info)}
+      </Swipeable>
+    )
+  }
+
+  render() {
+    const { info } = this.props;
+    return (
+      <div>{this.props.isMobile ? this.renderMobileInfo(info) : this.renderDesktopInfo(info)}</div>
     )
   }
 }
