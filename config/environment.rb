05cc62973354096e32fae643e160d11ac24e442b
Brian Heise <bnheise@gmail.com>
2020-06-01 08:39:52 +0900

    cloudinary configured

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the Rails application.
 require_relative 'application'
 
+# set cloudinary for external storage
+config.active_storage.service = :cloudinary
+
 # Initialize the Rails application.
 Rails.application.initialize!
