45eba10144dd0975afe4cfe5cf0101d457858480
Brian Heise <bnheise@gmail.com>
2020-06-01 08:44:50 +0900

    configured image upload

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,5 @@
 # Load the Rails application.
 require_relative 'application'
 
-# set cloudinary for external storage
-config.active_storage.service = :cloudinary
-
 # Initialize the Rails application.
 Rails.application.initialize!
