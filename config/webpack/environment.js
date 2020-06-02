05856d7e505d557395a6e8276bfad2c34297e5ea
Brian Heise <bnheise@gmail.com>
2020-06-02 22:04:03 +0900

    loader failed

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,8 +1,6 @@
 const { environment } = require('@rails/webpacker')
 const erb = require('./loaders/erb')
-const css = require('./loaders/css')
 
 environment.loaders.prepend('erb', erb)
-environment.loaders.prepend('css', css)
 environment.loaders.delete('nodeModules')
 module.exports = environment
