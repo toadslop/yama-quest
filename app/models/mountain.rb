07241632e414c3ab8ec4923fc30c4cdc78691b79
Brian Heise <bnheise@gmail.com>
2020-06-21 11:27:43 +0900

    added tests for the mountain model

--- app/models/mountain.rb
+++ app/models/mountain.rb
@@ -4,6 +4,8 @@ class Mountain < ApplicationRecord
   belongs_to :region, -> { select(:id, :name) }
   has_many :list_mountains
   has_many :lists, through: :list_mountains
+  validates_associated :region
+  validates :name, :altitude, :lat, :lng, presence: true
 
   # This returns the data necessary to render a feature as a point on mapbox map.
   def geojson_feature
