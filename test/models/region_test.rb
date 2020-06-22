fb93b70584e177ce687dd2169ac9865bd845fccc
Brian Heise <bnheise@gmail.com>
2020-06-23 06:54:14 +0900

    finally fixed pg errors maybe i hope

--- test/models/region_test.rb
+++ test/models/region_test.rb
@@ -3,20 +3,24 @@
 require 'test_helper'
 
 class RegionTest < ActiveSupport::TestCase
+  def setup
+    @region = Region.new(name: '北海道')
+  end
+
   test 'should have a name' do
     region = Region.new
     assert_not region.save, 'saved a region without a name'
   end
 
   test 'should have a unique name' do
-    existing_region = Region.first
-    double_region = Region.new(name: existing_region.name)
+    @region.save
+    double_region = Region.new(name: @region.name)
     assert_not double_region.save, 'saved a region that already exists'
   end
 
   test 'should retrieve mountains in the region' do
-    region = Region.first
-    assert region.mountains
+    @region.save
+    assert @region.mountains
   end
 
   def teardown
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -29,3 +29,19 @@ end
 class ActiveSupport::TestCase
   ActiveRecord::Migration.check_pending!
 end
+
+
+
+# ENV['RAILS_ENV'] ||= 'test'
+# require_relative '../config/environment'
+# require 'rails/test_help'
+
+# class ActiveSupport::TestCase
+#   # Run tests in parallel with specified workers
+#   parallelize(workers: :number_of_processors)
+
+#   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+#   fixtures :all
+
+#   # Add more helper methods to be used by all tests here...
+# end
