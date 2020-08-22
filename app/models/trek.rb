108639b09883fe769716fab36077213f2174d416
Brian Heise <bnheise@gmail.com>
2020-08-22 16:49:51 +0900

    saved trek, no changed

--- app/models/trek.rb
+++ app/models/trek.rb
@@ -7,8 +7,7 @@ class Trek < ApplicationRecord
     def self.prepare_gpx(gpx_url)
         trkps = Trek.get_gpx_trkpts(gpx_url)
         lat_array = Trek.get_lat_array(trkps)
-        Trek.get_lat_array(trkps)
-        # lng_array =
+        lng_array = Trek.get_lat_array(trkps)
     end
 
     def self.get_gpx_trkpts(gpx_url)
