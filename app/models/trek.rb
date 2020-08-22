4ec5729718dedd75884387b4e071241ab39246f4
Brian Heise <bnheise@gmail.com>
2020-08-22 16:28:55 +0900

    can parse gpx and return an array of longitudes and an array of lattitudes

--- app/models/trek.rb
+++ app/models/trek.rb
@@ -4,7 +4,28 @@ class Trek < ApplicationRecord
     belongs_to :user
     validates :name, presence: true
 
-    def parse_gpx(gpx_filepath)
+    def self.prepare_gpx(gpx_url)
+        trkps = Trek.get_gpx_trkpts(gpx_url)
+        lat_array = Trek.get_lat_array(trkps)
+        Trek.get_lat_array(trkps)
+        # lng_array =
+    end
+
+    def self.get_gpx_trkpts(gpx_url)
+        gpx = Nokogiri::XML(File.open(gpx_url))
+        gpx.search('trkpt')
+    end
+
+    def self.get_lat_array(trkpts)
+        trkpts.map do |trkpt| 
+            trkpt.attributes["lat"].value
+        end
+    end
+
+    def self.get_lat_array(trkpts)
+        trkpts.map do |trkpt| 
+            trkpt.attributes["lon"].value
+        end
     end
   end
   
