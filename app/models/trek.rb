3e61a5d93d29676c0ea6d1b0722500854b2e6d9e
Brian Heise <bnheise@gmail.com>
2020-08-23 17:24:35 +0900

    can extract mountains in the bounds

--- app/models/trek.rb
+++ app/models/trek.rb
@@ -4,5 +4,16 @@ class Trek < ApplicationRecord
     belongs_to :user
     has_one :gpx
     validates :name, presence: true
+
+    def mountains_in_bounds
+        gpx_data = gpx.prepare
+        p least_lat = gpx_data[:bounds][:lat_bounds][0]
+        p most_lat = gpx_data[:bounds][:lat_bounds][1]
+        p least_lng = gpx_data[:bounds][:lng_bounds][0]
+        p most_lng = gpx_data[:bounds][:lng_bounds][1]
+        Mountain.where(
+            "lat > #{least_lat} AND lat < #{most_lat} AND lng > #{least_lng} AND lng < #{most_lng}"
+        )
+    end
 end
   
