2f1ffd703ce4c5e9f5e6efd012f1acf8199130db
Brian Heise <bnheise@gmail.com>
2020-08-23 17:42:11 +0900

    in process of writing check summits method

--- app/models/trek.rb
+++ app/models/trek.rb
@@ -5,12 +5,28 @@ class Trek < ApplicationRecord
     has_one :gpx
     validates :name, presence: true
 
-    def mountains_in_bounds
+    def process_gpx
         gpx_data = gpx.prepare
-        p least_lat = gpx_data[:bounds][:lat_bounds][0]
-        p most_lat = gpx_data[:bounds][:lat_bounds][1]
-        p least_lng = gpx_data[:bounds][:lng_bounds][0]
-        p most_lng = gpx_data[:bounds][:lng_bounds][1]
+        trkpts = gpx_data[:trkpts]
+        bounds = gpx_data[:bounds]
+        mountains = mountains_in_bounds(bounds)
+        check_summits(mountains)
+    end
+
+    def check_summits(mountains, trkpts)
+        mountains.each do |mountain|
+            mountain_coords = {lng: mountain.lng, lat: mountain.lat}
+            trkpts.each do |trkpt|
+
+            end
+        end
+    end
+
+    def mountains_in_bounds(bounds)
+        least_lat = bounds[:lat_bounds][0]
+        most_lat = bounds[:lat_bounds][1]
+        least_lng = bounds[:lng_bounds][0]
+        most_lng = bounds[:lng_bounds][1]
         Mountain.where(
             "lat > #{least_lat} AND lat < #{most_lat} AND lng > #{least_lng} AND lng < #{most_lng}"
         )
