9ceeee85d29a1c1087be823fa55d11933c3ea88c
Brian Heise <bnheise@gmail.com>
2021-05-27 07:36:16 +0900

    commented out link

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# Controls the pages that display mountain lists
 class ListsController < ApplicationController
   def index
     @lists = List.all
--- app/javascript/explorer/containers/navbarElements/CenterNav.jsx
+++ app/javascript/explorer/containers/navbarElements/CenterNav.jsx
@@ -1,19 +1,19 @@
-import React, { PureComponent } from 'react';
+import React, { PureComponent } from "react";
 
 class CenterNav extends PureComponent {
-  render() {
-    const { mobileClass} = this.props
-    return (
-      <div className={`center-group ${mobileClass}`}>
-        <i className="fas fa-mountain nav-icon"></i>
-        <i className="fas fa-user nav-icon"></i>
-        <a href='/lists'>
-          <i className="fas fa-scroll nav-icon"></i>
-        </a>
-        <i className="fas fa-home nav-icon"></i>
-      </div>
-    );
-  }
+	render() {
+		const { mobileClass } = this.props;
+		return (
+			<div className={`center-group ${mobileClass}`}>
+				<i className="fas fa-mountain nav-icon"></i>
+				<i className="fas fa-user nav-icon"></i>
+				{/* <a href='/lists'> */}
+				<i className="fas fa-scroll nav-icon"></i>
+				{/* </a> */}
+				<i className="fas fa-home nav-icon"></i>
+			</div>
+		);
+	}
 }
 
 export default CenterNav;
